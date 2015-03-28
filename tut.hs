b6ce0a0c90f9de5be35656beddf5e8c24e1cd3c9
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-03-27 21:19:14 -0500

    Up to Guards, guards in syntax-in-functions

--- tut.hs
+++ tut.hs
@@ -9,6 +9,62 @@ doubleSmallBabyChild' x = (if x > 100 then x else x * 2) + 1
 
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 
-length' xs = sum [1 | _ <- xs]
+--length' xs = sum [1 | _ <- xs]
+--
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_:xs) = 1 + length' xs
 
+capital :: String -> String
+captial "" = "Empty string, whoops!"
+capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
+
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
+
+removeNonUppercase :: [Char] -> [Char]
 removeNonUppercase st = [ c | c <-st, c `elem` ['A'..'Z']]
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
+
+lucky :: (Integral a) => a -> String
+lucky 7 = "Lucky number seven!"
+lucky x = "Sorry, you're out of luck, pal!"
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n-1)
+
+head' :: [a] -> a
+head' [] = error "I love it when tutorials call me dumb"
+head' (x:_) = x
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+tell (x:[]) = "The list has one element " ++ show x
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "this list is too long. First and second element are :" ++ show x ++ "and " ++ show y
+
+--calcMod :: Int -> (Int, Int)
+--calcMod x = (x `mod` 3, x `mod` 5)
+
+--fizzBuzz :: (Int, Int) -> String
+--fizzBuzz :: (0, 0) = "FizzBuzz"
+--fizzBuzz :: (0, x) = "Fizz"
+--fizzBuzz :: (x, 0) = "Buzz"
+--fizzBuzz :: (x, y) = show (x * 3)
+
+--fizzBuzz :: (Integral a) => a -> String
+--fizzBuzz a `mod` 3 == 0 + a `mod` 5 == 0 = "FizzBuzz"
+--fizzBuzz a `mod` 3 == 0 = "Fizz"
+--fizzBuzz a `mod` 5 == 0 = "Buzz"
+--fizzBuzz a = show a 
