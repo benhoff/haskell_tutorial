d84c9750b7aa10cae023019602674230b561d68b
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-04-06 19:53:48 -0500

    Up to Case Expressions in synatx-in-functions. Successfully compiles!

--- tut.hs
+++ tut.hs
@@ -54,22 +54,20 @@ tell (x:[]) = "The list has one element " ++ show x
 tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
 tell (x:y:_) = "this list is too long. First and second element are :" ++ show x ++ "and " ++ show y
 
-{-bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell :: (RealFloat a) => a -> a -> String
 bmiTell weight height
   | bmi <= skinny = "You're underweight, that can be hard, sorry!"
   | bmi <= normal = "You're supposedly nomral. Bet you're not though!"
-  | bmi <= fat    = "You're fat! Lose some weight!"
+  | bmi <= fat   = "You're fat! Lose some weight!"
   | otherwise     = "you're undeniable!"
-  where 
-    bmi = weight / height ^ 2
-    (skinny, normal, fat) = (18.5, 25.0, 30.0)
+  where bmi = weight / height ^2
+        (skinny, normal, fat) = (18.5, 25.0, 30.0)
 
 initials :: String -> String -> String
 initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
   where 
     (f:_) = firstname
     (l:_) = lastname
-
 myCompare :: (Ord a) => a -> a -> Ordering
 a `myCompare` b
   | a > b   = GT
@@ -77,19 +75,18 @@ a `myCompare` b
   | otherwise = LT
 
 calcBmis :: (RealFloat a) => [(a, a)] -> [a]
-calcBmis xs = [bmi w h | (w, h) <- xs, let bmi = w / h ^ 2]
+calcBmis xs = [bmi | (w, h) <- xs, let bmi = w / h ^2]
 
 cylinder :: (RealFloat a) => a -> a -> a
 cylinder r h =
   let 
     sideArea = 2 * pi * r * h
-    topArea = pi * r ^2
+    topArea = pi * r 
   in 
     sideArea + 2 * topArea
 
 max' :: (Ord a) => a -> a -> a
 max' a b | a > b = a | otherwise = b
--}
 
 --max' :: (Ord a) => a -> a -> a
 --max' a b
@@ -98,12 +95,6 @@ max' a b | a > b = a | otherwise = b
 
 --calcMod :: Int -> (Int, Int)
 --calcMod x = (x `mod` 3, x `mod` 5)
-
---fizzBuzz :: (Int, Int) -> String
---fizzBuzz :: (0, 0) = "FizzBuzz"
---fizzBuzz :: (0, x) = "Fizz"
---fizzBuzz :: (x, 0) = "Buzz"
---fizzBuzz :: (x, y) = show (x * 3)
 
 fizzBuzz' :: (Integral a, Show a) => a -> String
 fizzBuzz' a 
